1d43898ab3bb66082cd3dfa6ed266e7afd8870f6
mmena2017 <mmena2017@fau.edu>
2019-02-12 21:35:36 -0500

    Blocks can now be moved with the mouse. Sorting order will need work.

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -1,23 +1,65 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-
 public class Block : MonoBehaviour
 {
-    [SerializeField]Text text;
-    [SerializeField] Slot slot;
+    [SerializeField] bool isDragged = false;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //cache
+    [SerializeField] Text text;
+    [SerializeField] Canvas canvas;
+    [SerializeField] SpriteRenderer ren;
 
     // Update is called once per frame
     void Update()
     {
-        
+        drag();
+        checkifstillDragged();
+    }
+
+    private void drag()
+    {
+        if (isDragged)
+        {
+            SnaptoMouse();
+            SetSortingOrders(10);
+        }
+    }
+
+    private void checkifstillDragged()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            if (isDragged)//last dragged block stays on top
+            {
+                SetSortingOrders(1);
+            }
+            else
+            {
+                SetSortingOrders(0);
+            }
+            isDragged = false;
+        }
+    }
+
+    private void SetSortingOrders(int x)
+    {
+        ren.sortingOrder = canvas.sortingOrder = x;
+    }
+
+    private void SnaptoMouse()
+    {
+        Vector3 mousepos_V3 = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 mousepos_V2 = new Vector2(mousepos_V3.x, mousepos_V3.y);
+
+        this.transform.position = mousepos_V2;
+    }
+
+    public void beingDragged()
+    {
+        isDragged = true;
     }
 }
---
+++ Assets/Scripts/Controls.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controls : MonoBehaviour
+{
+    [SerializeField] LayerMask selectableMask;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousepos_V3 = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 mousepos_V2 = new Vector2(mousepos_V3.x, mousepos_V3.y);
+
+            RaycastHit2D hit;
+
+            if (hit = Physics2D.Raycast(mousepos_V2, Vector2.zero, selectableMask))
+            {
+                hit.collider.GetComponent<Block>().beingDragged();
+            }
+        }
+    }
+}
