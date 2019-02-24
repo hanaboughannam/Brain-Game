0e5398a07e9af485af0b6af2f0bd76bab44d19ad
mmena2017 <mmena2017@fau.edu>
2019-02-23 19:25:40 -0500

    A Slot block's value is viewable from the inpector

--- Assets/app/mathgame/Scripts/Editor/SlotViewer.cs
+++ Assets/app/mathgame/Scripts/Editor/SlotViewer.cs
@@ -3,7 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
-public class SlotViewer : MonoBehaviour
+[CustomEditor(typeof(Slot))]
+public class SlotViewer : Editor
 {
-    
+    public override void OnInspectorGUI()
+    {
+        //base.OnInspectorGUI();
+        Slot slot = (Slot)target;
+
+        EditorGUILayout.LabelField("My Block's Value = " + slot.getValue());
+    }
 }
