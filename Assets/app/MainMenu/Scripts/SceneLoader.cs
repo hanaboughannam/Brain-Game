0d7b2b4433916a7b6a39ea602808efa4a8a66e14
mmena2017 <mmena2017@fau.edu>
2019-03-23 18:43:46 -0400

    Added new/tempory logic to the main menu to support a release

---
+++ Assets/app/MainMenu/Scripts/SceneLoader.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void loadPlayerSetup()
+    {
+        SceneManager.LoadScene("PlayerSetup");
+    }
+    /// <summary>
+    /// Loads the next scene based on build settings
+    /// </summary>
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void ReloadScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
--- Assets/app/MainMenu/Scripts/Welcome.cs
+++ Assets/app/MainMenu/Scripts/Welcome.cs
@@ -9,4 +9,5 @@ public class Welcome : MonoBehaviour
     {
         UnityEngine.SceneManagement.SceneManager.LoadScene("PlayerSetup");
     }
+
 }
