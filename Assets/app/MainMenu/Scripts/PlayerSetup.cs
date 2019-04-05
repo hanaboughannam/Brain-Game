9af0ea71483df710a93b9ee78ec0dc9f5ad93606
Daniel Mirabal <dan.e.mirabal@gmail.com>
2019-04-04 21:44:56 -0400

    Added New Menus/scenes. Changed the look of the UI.

--- Assets/app/MainMenu/Scripts/PlayerSetup.cs
+++ Assets/app/MainMenu/Scripts/PlayerSetup.cs
@@ -2,17 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Setup : MonoBehaviour
+public class PlayerSetup : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void loadPlayerSetup()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        UnityEngine.SceneManagement.SceneManager.LoadScene("PlayerProfile");
     }
 }
---
+++ Assets/app/MainMenu/Scripts/SetName.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+
+public class SetName : MonoBehaviour
+{   
+    public InputField inputText;
+    string userName;
+    public Text prompt;
+    
+    void Start() {
+        userName = PlayerPrefs.GetString("userName");
+        inputText.text = userName;
+        if(userName != ""){
+            Destroy(prompt);
+        }
+    }
+
+    public void SaveName() {
+        name = inputText.text;
+        PlayerPrefs.SetString("userName", name);
+    }
+}
